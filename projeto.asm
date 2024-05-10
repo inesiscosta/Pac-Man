2f7279398ec12b352fc3d580ce06edbd99cb87a8
mc8mac <marcosmachadomcm@gmail.com>
2024-05-10 15:53:04 +0100

    fixed keyboard bugs

--- projeto.asm
+++ projeto.asm
@@ -42,42 +42,44 @@ keyboard:
     PUSH R6;
     PUSH R7;
 
-    MOV R2, TEC_L
-    MOV R3, TEC_C
-    MOV R4, DISPLAYS
-    MOV R5, MASCARA_TEC
+    MOV R2, TEC_L       ; Endereço do periférico das linhas do teclado
+    MOV R3, TEC_C       ; Endereço do periférico das colunas do teclado
+    MOV R4, DISPLAYS    ; Endereço do periférico dos displays
+    MOV R5, MASCARA_TEC ; Máscara para a leitura do teclado
 
-    reset_line:
-        MOV R1, LINHA
-        MOV R7, 0
+    reset_line: 
+        MOV R1, LINHA   ; define a linha a ler (inicialmente a 1)
+        MOV R7, 0       ; Inicializa o contador de linhas a 0
 
     check_key:
-        MOVB [R2], R1
-        MOVB R0, [R3]
-        AND R0, R5
-        CMP R0, 0
-        JZ next_line
-        JMP is_key_pressed
+        MOVB [R2], R1       ; Ativa a linha para leitura do teclado
+        MOVB R0, [R3]       ; Lê a coluna do teclado
+        AND R0, R5          ; Aplica a máscara
+        CMP R0, 0           ; Verifica se alguma tecla foi pressionada
+        JZ next_line        ; Se não foi pressionada, passa para a próxima linha
+        PUSH R0
+        JMP is_key_pressed  ; caso contrário, espera que deixe de ser pressionada
 
     next_line:
-        SHL R1, 1
-        INC R7
-        CMP R7, 4
-        JNZ check_key
-        JMP reset_line
+        SHL R1, 1      ; Passa para a próxima linha
+        INC R7         ; Incrementa o contador de linhas
+        CMP R7, 4      ; Verifica se já leu todas as linhas
+        JNZ check_key  ; Se não leu todas as linhas, verifica a próxima
+        JMP reset_line ; Caso contrário, reinicia a linha
 
-    is_key_pressed:
-        MOVB [R2], R1
-        MOVB R0, [R3]
-        AND R0, R5
-        CMP R0, 0
-        JNZ is_key_pressed
-        JMP keyboard_end
+    is_key_pressed: 
+        MOVB [R2], R1      ; Ativa a linha para leitura do teclado
+        MOVB R0, [R3]      ; Lê a coluna do teclado
+        AND R0, R5         ; Aplica a máscara
+        CMP R0, 0          ; Verifica se a tecla foi libertada
+        JNZ is_key_pressed ; Se não foi libertada, espera que seja
+        JMP keyboard_end   ; Termina a rotina
 
     keyboard_end:
-        SHL R7, 2
-        OR R7, R0
-        CALL keyboard_command
+        POP R0             ; Termina a rotina
+        SHL R7, 2             ; Multiplica o número da linha por 4
+        ADD R7, R0             ; Adiciona o número da coluna
+        CALL keyboard_command ; Executa o comando da tecla pressionada
 
     POP R7
     POP R6
@@ -97,6 +99,7 @@ keyboard_command:
     JZ kc_increment
     CMP R7, 2
     JZ kc_decrement
+    JMP kc_end
     
     kc_increment:
         ; implement Routine
